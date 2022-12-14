c1651005fa8da75b1386ab84c9240f07f8c8645c
mj <jaczewski147@gmail.com>
2022-12-14 20:01:52 +0100

    add percent chart bar value

--- src/app/components/chart/chart-bar/chart-bar.component.ts
+++ src/app/components/chart/chart-bar/chart-bar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chart-bar',
-  templateUrl: './chart-bar.component.html',
-  styleUrls: ['./chart-bar.component.scss']
+	selector: 'app-chart-bar',
+	templateUrl: './chart-bar.component.html',
+	styleUrls: ['./chart-bar.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartBarComponent implements OnInit {
+	@Input() label: string = '';
+	@Input() group: number = 1;
+	@Input() set percent(percent: number) {
+		this.percentString = this.createPercentValue(percent);
+	}
 
-  constructor() { }
+	percentString = this.createPercentValue(0);
 
-  ngOnInit(): void {
-  }
+	constructor() {}
 
+	ngOnInit(): void {}
+
+	getCssColorClass(): string {
+		switch (this.group) {
+			case 1:
+			default:
+				return 'background-primary';
+			case 2:
+				return 'background-secondary';
+		}
+	}
+
+	createPercentValue(number: number): string {
+		if (number < 0.1) {
+			number = 0.1;
+		}
+
+		return `${number}%`;
+	}
 }
